c1cdb5984930c13f825b3c43c2b6e29e8cd57f78
bmmuceo <bmmuceo@gmail.com>
2025-04-16 13:14:44 -0400

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by RyanA on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
